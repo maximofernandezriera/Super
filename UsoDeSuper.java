f249c95d5b513d176da30a67c53b61f6da9cd2c7
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2023-02-24 19:02:56 +0100

    Add files via upload

---
+++ UsoDeSuper.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package usodesuper;
+
+/**
+ *
+ */
+public class UsoDeSuper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ClaseBase base = new ClaseBase();
+		ClaseDerivada derivada = new ClaseDerivada();
+//		ClaseDerivada derivada = new ClaseDerivada("Hola Mundo!"," desde una clase derivada");
+		
+		
+		base.imprimir();
+		System.out.println("---------------");
+		derivada.imprimir();
+		
+
+	}
+
+}
